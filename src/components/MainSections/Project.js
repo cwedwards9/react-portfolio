84a798d57c209833a8f73452d3084471706bbd0f
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-25 12:14:19 -0500

    Removed isStarred field

--- src/components/MainSections/Project.js
+++ src/components/MainSections/Project.js
@@ -2,14 +2,12 @@ import React, {Component} from "react";
 
 class Project extends Component {
     render() {
-        const { image, alt, isStarred, name, desc, appLink, repoLink } = this.props;
+        const { image, alt, name, desc, appLink, repoLink } = this.props;
         return (
             <div className="card">
                 <img src={image} className="card-img-top" alt={alt} />
                 <div className="card-body">
-                    <h5 className="card-title">{name} 
-                        {isStarred ? <i className="fas fa-star"></i> : "" }
-                    </h5>
+                    <h5 className="card-title">{name}</h5>
                     <p className="card-text">{desc}</p>
                     <a href={appLink} target="_blank" rel="noreferrer"className="app-link"><i className="fas fa-link"></i> App</a>
                     <a href={repoLink} target="_blank" rel="noreferrer" className="repo-link"><i className="far fa-folder-open"></i> Repo</a>
--- src/components/MainSections/ProjectList.js
+++ src/components/MainSections/ProjectList.js
@@ -13,7 +13,6 @@ class ProjectList extends Component {
                 id: 1,
                 image: smartMoneyImg,
                 alt: "A dashboard that shows the user's budget, a list of their inputted transactions, and a graph to represent their spending by category.",
-                isStarred: true,
                 name: "Budget Tracker",
                 desc: "A full-stack app used for tracking monthly transactions and bills to pay.",
                 appLink: "https://smart-money-app.herokuapp.com/",
@@ -23,17 +22,15 @@ class ProjectList extends Component {
                 id: 2,
                 image: petItImg,
                 alt: "A post of a kitten resting in a persons arm.",
-                isStarred: false,
                 name: "PetIt - Social Media App (In development)",
                 desc: "A social media for users to post pictures of their pets. All pets welcome!",
-                appLink: "",
+                appLink: "https://pet-it.herokuapp.com/",
                 repoLink: "https://github.com/cwedwards9/social-media-pet-app"
             },
             {
                 id: 3,
                 image: fitnessTracker,
                 alt: "Two graphs, a line graph and a bar graph and two circle charts below measuring performance of workouts and exercises.",
-                isStarred: false,
                 name: "Fitness Tracker",
                 desc: "An app that allows a user to create and track workouts.",
                 appLink: "https://fitness-tracking-application.herokuapp.com/stats",
@@ -43,7 +40,6 @@ class ProjectList extends Component {
                 id: 4,
                 image: employeedDirectoryImg,
                 alt: "A heading that reads 'Employee Directory' and below it is a list of employees names and their picture.",
-                isStarred: false,
                 name: "Employee Directory",
                 desc: "An employee list that can be sorted and filtered based on user input, made with React.",
                 appLink: "https://cwedwards9.github.io/employee-directory-react/",
@@ -57,7 +53,6 @@ class ProjectList extends Component {
                 key={proj.id}
                 image={proj.image}
                 alt={proj.alt}
-                isStarred={proj.isStarred}
                 name={proj.name}
                 desc={proj.desc}
                 appLink={proj.appLink}
