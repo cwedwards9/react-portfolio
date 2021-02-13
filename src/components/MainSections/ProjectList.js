ddb2ada8f47e131d3556e7cfa659f9d2e546de85
Chase Edwards <chase.w.edwards9@gmail.com>
2021-02-13 14:24:27 -0500

    Move the projects list and their data to a new file.

--- src/components/MainSections/ProjectList.js
+++ src/components/MainSections/ProjectList.js
@@ -1,54 +1,12 @@
 import React, {Component} from "react";
 import Project from "./Project";
 import "./ProjectList.css";
-import smartMoneyImg from "../../assets/smart-money-img.PNG";
-import bookSearch from "../../assets/book-search-img.PNG";
-import petItImg from "../../assets/petit-img.PNG";
-import employeedDirectoryImg from "../../assets/employee-directory-img.PNG";
+import { projectData } from "../ProjectData";
 
 class ProjectList extends Component {
-    static defaultProps = {
-        projects: [
-            {
-                id: 1,
-                image: smartMoneyImg,
-                alt: "A dashboard that shows the user's budget, a list of their inputted transactions, and a graph to represent their spending by category.",
-                name: "Budget Tracker",
-                desc: "A full-stack app used for tracking monthly transactions and bills to pay.",
-                appLink: "https://smart-money-app.herokuapp.com/",
-                repoLink: "https://github.com/cwedwards9/budget-tracker"
-            },
-            {
-                id: 2,
-                image: petItImg,
-                alt: "A post of a kitten resting in a persons arm.",
-                name: "PetIt - Social Media App (In development)",
-                desc: "A social media for users to post pictures of their pets. All pets welcome!",
-                appLink: "https://pet-it.herokuapp.com/",
-                repoLink: "https://github.com/cwedwards9/social-media-pet-app"
-            },
-            {
-                id: 3,
-                image: bookSearch,
-                alt: "A search bar with the word, 'Lord of the Flies' typed into it and a result for the book with a description and the book cover.",
-                name: "Book Search",
-                desc: "An app that allows a user to search for book titles, save them, and refer back to them later.",
-                appLink: "https://books-search-react-app.herokuapp.com/",
-                repoLink: "https://github.com/cwedwards9/book-app"
-            },
-            {
-                id: 4,
-                image: employeedDirectoryImg,
-                alt: "A heading that reads 'Employee Directory' and below it is a list of employees names and their picture.",
-                name: "Employee Directory",
-                desc: "An employee list that can be sorted and filtered based on user input, made with React.",
-                appLink: "https://cwedwards9.github.io/employee-directory-react/",
-                repoLink: "https://github.com/cwedwards9/employee-directory-react"
-            }
-        ]
-    }
+    
     render() {
-        const projects = this.props.projects.map(proj => (
+        const projects = projectData.map(proj => (
             <Project
                 key={proj.id}
                 image={proj.image}
---
+++ src/components/ProjectData.js
@@ -0,0 +1,44 @@
+import ModernLivingImg from "../assets/modern-living-img.PNG"
+import smartMoneyImg from "../assets/smart-money-img.PNG";
+import petItImg from "../assets/petit-img.PNG";
+import bookSearch from "../assets/book-search-img.PNG";
+
+
+export const projectData = [
+    {
+        id: 1,
+        image: ModernLivingImg,
+        alt: "A heading that reads 'Starter Guides' and below it are four squares for Kitchen, Bathroom, Living Room, and Media Room.",
+        name: "Modern Living - Home Improvement App",
+        desc: "A centralized place for all things needed for your home improvement.",
+        appLink: "https://modern-living.herokuapp.com/",
+        repoLink: "https://github.com/cwedwards9/modern-living-app"
+    },
+    {
+        id: 2,
+        image: smartMoneyImg,
+        alt: "A dashboard that shows the user's budget, a list of their inputted transactions, and a graph to represent their spending by category.",
+        name: "Smart Money - Budget Tracker",
+        desc: "A full-stack app used for tracking monthly transactions and bills to pay.",
+        appLink: "https://smart-money-app.herokuapp.com/",
+        repoLink: "https://github.com/cwedwards9/budget-tracker"
+    },
+    {
+        id: 3,
+        image: petItImg,
+        alt: "A post of a kitten resting in a persons arm.",
+        name: "PetIt - Social Media App",
+        desc: "A social media for users to post pictures of their pets. All pets welcome!",
+        appLink: "https://pet-it.herokuapp.com/",
+        repoLink: "https://github.com/cwedwards9/social-media-pet-app"
+    },
+    {
+        id: 4,
+        image: bookSearch,
+        alt: "A search bar with the word, 'Lord of the Flies' typed into it and a result for the book with a description and the book cover.",
+        name: "Book Search",
+        desc: "An app that allows a user to search for book titles, save them, and refer back to them later.",
+        appLink: "https://books-search-react-app.herokuapp.com/",
+        repoLink: "https://github.com/cwedwards9/book-app"
+    }
+];
