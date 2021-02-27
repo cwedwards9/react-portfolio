e170dbf27a8d655928f4612f5b362ec1b31bb062
Chase Edwards <chase.w.edwards9@gmail.com>
2021-02-27 16:20:28 -0500

    Remove hover pseudo class for icons and add more margin between the icons.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,9 +13,9 @@ class Footer extends Component {
                     <p>Feel free to check out my other links...</p>
                 </div>
                 <div className="socialLinks">
-                    <a href="https://www.linkedin.com/in/chase-w-edwards/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin fa-2x mr-1"></i></a>
+                    <a href="https://www.linkedin.com/in/chase-w-edwards/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin fa-2x mr-2"></i></a>
                     <a href="https://github.com/cwedwards9" target="_blank" rel="noreferrer"><i className="fab fa-github-square fa-2x my-1"></i></a>
-                    <a href={pdf} target="_blank" rel="noreferrer"><i className="fas fa-file-alt fa-2x ml-1"></i></a>
+                    <a href={pdf} target="_blank" rel="noreferrer"><i className="fas fa-file-alt fa-2x ml-2"></i></a>
                 </div>
             </footer>
         );
