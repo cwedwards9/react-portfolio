2e2028322629441aaa0c60848c60a5ac49ca45aa
Chase Edwards <chase.w.edwards9@gmail.com>
2021-03-17 15:30:25 -0400

    Revise content in paragraphs.

--- src/components/MainSections/About.js
+++ src/components/MainSections/About.js
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 import "./About.css";
 import chasePic from "../../assets/chase-pic.jpg";
-// import CollegeGradPic from "../../assets/college-grad.jpg"
 
 class About extends Component {
     render() {
@@ -18,7 +17,7 @@ class About extends Component {
                         Information Systems when I was exposed to programming and database management. This was not the core concentration
                         of my major, but it opened my eyes to the world of technology. Since then, I have earned my degree and I have earned
                         a certificate in Full Stack Web Development from Case Western Reserve University. I have also completed several individual and group projects 
-                        where I was able to utilize my skills I have learned along the way to develop full-stack web applications I am proud of. I am
+                        where I was able to utilize my skills I have learned along the way to develop full-stack web applications. I am
                         a hard worker, naturally curious, and dedicated to learning new things.
                         </p>
                     </div>
