11a0c0b35db6a847192300df08290cabb185e4c0
Chase Edwards <chase.w.edwards9@gmail.com>
2021-04-05 14:51:13 -0400

    Initial commit

---
+++ src/mainSections/Tech.js
@@ -0,0 +1,40 @@
+import React from "react";
+import './Tech.css';
+import { VscCode } from "react-icons/vsc"
+
+export default function Tech() {
+    return (
+      <div className="technologies">
+        <section className="front-end-tech" style={{borderRight: "1px solid lightgray"}}>
+            <i class="fas fa-code tech-icon"></i>
+            <h2>Front-End Skills</h2>
+            <p>I value accessible sites for everyone, bringing an intuitive and clean design with thoughtful interactions.</p>
+            <h3>Languages and Technologies:</h3>
+            <ul>
+                <li>HTML / CSS</li>
+                <li>SASS / SCSS</li>
+                <li>EJS / Handlebars</li>
+                <li>JavaScript / jQuery</li>
+                <li>Bootstrap</li>
+                <li>React</li>
+            </ul>
+        </section>
+
+        <section className="back-end-tech">
+            <i class="fas fa-server tech-icon"></i>    
+            <h2>Back-End Skills</h2>
+            <p>I value keeping code organized and modular. Security and efficiency are always a priority.</p>
+            <h3>Languages and Technologies:</h3>
+            <ul>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>SQL / MySQL</li>
+                <li>Sequelize</li>
+                <li>MongoDB</li>
+                <li>Mongoose</li>
+                
+            </ul>
+        </section>
+      </div>
+    );
+}
