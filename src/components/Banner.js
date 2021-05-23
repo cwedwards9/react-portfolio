542ba2653a593ec63419bcc50fd474df84c63146
Chase Edwards <chase.w.edwards9@gmail.com>
2021-05-23 18:39:40 -0400

    Edit content.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,7 @@ export default function Banner() {
             <div className="background-image"></div>
             <div className="hero-content-area">
                 <h1>Hello! I'm Chase.</h1>
-                <h3>Full Stack Web Developer</h3>
+                <h3>Web Developer</h3>
                 <a href={pdf} target="_blank" rel="noreferrer">View My Resume</a>
             </div>
         </header>
--- src/mainSections/About.js
+++ src/mainSections/About.js
@@ -9,10 +9,9 @@ export default function About() {
             <div className="aboutDesc">
                 <p>My interest in software and web development started back in 2018. I was earning my undergraduate degree in 
                 Information Systems when I was exposed to programming and database management. This was not the core concentration
-                of my major, but it opened my eyes to the world of technology. Since then, I have earned my degree and I have earned
-                a certificate in Full Stack Web Development from Case Western Reserve University. I have also completed several individual and group projects 
-                where I was able to utilize my skills I have learned along the way to develop full-stack web applications, and I'm ready to 
-                put that experience to use as a front-end or full-stack web developer.
+                of my major, but it opened my eyes to the world of technology. Since then, I have earned my degree as well as a
+                certificate in Full Stack Web Development from Case Western Reserve University. I have completed several individual 
+                and group projects along the way and I am now a Web Development Intern learning everyday and loving it!
                 </p>
             </div>
         </div>
